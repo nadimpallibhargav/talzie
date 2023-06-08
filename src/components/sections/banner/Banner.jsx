5772116154c0c0d863ad198f50f9ef63a54904a7
sharan.km <sharan.km@ziniosedge.com>
2023-06-08 21:36:15 +0530

    banner section updated

--- src/components/sections/banner/Banner.jsx
+++ src/components/sections/banner/Banner.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-
+import priceTag from '../../../assets/images/price_tag.svg'
+import heroPic from '../../../assets/images/heroPic1.svg'
 import "./Banner.scss";
 
 const Banner = () => {
   return (
-    <section>
+    <section className="bannerSection">
       <div className="container">
         <div className="bannerContent">
+          <labe className="heroLabel">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, nobis<img className="priceTag" src={priceTag} alt="priceTag"/></labe>
           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati,
             nobis?
           </p>
+          <div className="heroImageContainer">
+            <img src={heroPic} alt="heroSectionImage" />
+          </div>
         </div>
       </div>
     </section>
