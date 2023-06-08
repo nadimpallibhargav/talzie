f708dc536b5e4f63adedbb92f6f0eae0064b2e20
nadimpallibhargav <bhargavraju98@gmail.com>
2023-06-08 15:23:07 +0530

    hiring section desktop

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import Banner from "./components/sections/banner/Banner";
 import Clients from "./components/sections/clients/Clients";
+import Hiring from "./components/sections/hiring/Hiring";
 import HowWeDo from "./components/sections/howwedo/HowWeDo";
 import Services from "./components/sections/services/Services";
 import Whyus from "./components/sections/whyus/Whyus";
@@ -16,6 +17,7 @@ function LandingPage() {
         <Services tabs={tabs} autoCycleInterval={3000} />
         <Whyus />
         <HowWeDo />
+        <Hiring />
       </main>
     </>
   );
