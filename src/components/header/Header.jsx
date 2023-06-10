3d5a1fbb7364bfbb04e3fe10e45f61a92123f397
nadimpallibhargav <bhargavraju98@gmail.com>
2023-06-10 12:59:23 +0530

    header mobile toggle added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../public/talzie_logo.svg";
 import "./Header.scss";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <div className="container">
         <nav>
           <div className="logoAndTitle">
-            <img src={logo} />
+            <img src={logo} alt="Logo" />
             <a className="logo" href="">
               ALZIE
             </a>
           </div>
-          <ul>
+          <div className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+          <ul className={`menu ${isMenuOpen ? 'open' : ''}`}>
             <li>
               <a href="">CLIENTS</a>
             </li>
