fd7764220ac747259cc76fe2364fb4de9fbc1ec6
nadimpallibhargav <bhargavraju98@gmail.com>
2023-06-21 23:58:29 +0530

    grammer update

--- src/components/sections/howwedo/HowWeDo.jsx
+++ src/components/sections/howwedo/HowWeDo.jsx
@@ -11,7 +11,7 @@ const HowWeDo = ({howWeDo}) => {
           <div className="howWeDoMainHeading">
             <div className="heading">
                 <h2>How We Do</h2>
-                <p>From  Onboarding to take off</p>
+                <p>From onboarding to takeoff</p>
                 <PrimaryButton btnName='Contact Us' />
             </div>
           </div>
@@ -21,7 +21,7 @@ const HowWeDo = ({howWeDo}) => {
               <div className="stepContent">
                 <img src="https://uploads-ssl.webflow.com/61a9d163662d2084d7cbc38f/636b5475cbddd71f7b9318f2_kickoff.svg" alt="how we do" />
                 <h3>Client Onboarding</h3>
-                <p>Understanding Client Requirements and Crafting a Strategy</p>
+                <p>Understanding client requirements and crafting a strategy</p>
               </div>
             </div>
             <div className="howWeDoCard">
@@ -29,7 +29,7 @@ const HowWeDo = ({howWeDo}) => {
               <div className="stepContent">
                 <img src="https://uploads-ssl.webflow.com/61a9d163662d2084d7cbc38f/636b5475cbddd71f7b9318f2_kickoff.svg" alt="how we do" />
                 <h3>Design Review</h3>
-                <p>Designing with Precision and Creativity</p>
+                <p>Designing with precision and creativity</p>
               </div>
             </div>
             <div className="howWeDoCard">
