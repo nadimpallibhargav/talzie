abf1467f55f2615148f07c7273766808a076dffa
nadimpallibhargav <bhargavraju98@gmail.com>
2023-06-21 13:46:15 +0530

    banner styles & colors added

--- src/components/sections/bannerV2/BannerV2.jsx
+++ src/components/sections/bannerV2/BannerV2.jsx
@@ -12,34 +12,30 @@ function BannerV2({ banner }) {
         <div className="priceTag">
           <p ref={banner}>
             <span>Limited Time Offer</span>. We are offering an exclusive
-            website for your business at an unbeatable price of{" "}
-            <span>9,999</span>
+            website for your business at an unbeatable price of
+            <span> 9,999 </span>
           </p>
         </div>
         <div className="container">
-          <div className="wrapper">
-            <div className="bannerWrapper">
-              <div className="content">
-                <h5 className="title">
-                  We provide clients with stunning <span>Talzie</span> web
-                  solutions, developed with <span>skills & passion.</span>
-                </h5>
-                <div className="aboutUs">
-                  <h4>About Us</h4>
-                  <p>
-                    A vibrant, dynamic, creative designers and developers who
-                    constantly work to bring your ideas to the table. We can
-                    deliver some mind-boggling outcomes.
-                  </p>
-                </div>
-                <Lottie
-                      animationData={LottieScroll}
-                      loop={true}
-                      style={{width: 50 , margin:"auto"}}
-                    />
-              </div>
-            </div>
+          <h5 className="title">
+            We provide clients with stunning <span> Talzie </span> web
+            solutions, developed with <span> skills & passion. </span>
+          </h5>
+        </div>
+        <div className="container">
+          <div className="aboutUs">
+            <h4>About Us</h4>
+            <p>
+              A vibrant, dynamic, creative designers and developers who
+              constantly work to bring your ideas to the table. We can deliver
+              some mind-boggling outcomes.
+            </p>
           </div>
+          <Lottie
+            animationData={LottieScroll}
+            loop={true}
+            style={{ width: 50, marginInline: "auto" }}
+          />
         </div>
       </section>
     </>
